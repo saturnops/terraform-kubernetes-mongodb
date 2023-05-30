946715e7c7233b7b6f79e541acd4feb2a3ef2a09
Amine Ballalou <ballalouahmedamine@gmail.com>
2023-05-30 23:26:43 +0530

    Initial Release

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ This module is for deploying a highly available MongoDB cluster on Kubernetes us
 
 ```hcl
 module "mongodb" {
-  source                   = "https://github.com/sq-ia/terraform-kubernetes-mongodb.git"
+  source                   = "saturnops/mongodb/kubernetes"
   cluster_name             = "dev-skaf"
   mongodb_config = {
   name               = "skaf"
@@ -48,10 +48,10 @@ module "mongodb" {
 
 
 ```
-Refer [examples](https://github.com/sq-ia/terraform-kubernetes-mongodb/tree/main/examples/complete) for more details.
+Refer [examples](https://github.com/RohitSquareops/terraform-kubernetes-mongodb/tree/main/examples/complete) for more details.
 
 ## IAM Permissions
-The required IAM permissions to create resources from this module can be found [here](https://github.com/sq-ia/terraform-kubernetes-mongodb/blob/main/IAM.md)
+The required IAM permissions to create resources from this module can be found [here](https://github.com/RohitSquareops/terraform-kubernetes-mongodb/blob/main/IAM.md)
 
 ## Important Notes
   1. In order to enable the exporter, it is required to deploy Prometheus/Grafana first.
--- example/complete/main.tf
+++ example/complete/main.tf
@@ -10,7 +10,7 @@ locals {
 }
 
 module "mongodb" {
-  source       = "https://github.com/sq-ia/terraform-kubernetes-mongodb.git"
+  source       = "saturnops/mongodb/kubernetes"
   cluster_name = "dev-cluster"
   mongodb_config = {
     name               = local.name
@@ -23,16 +23,16 @@ module "mongodb" {
   }
   mongodb_backup_enabled = true
   mongodb_backup_config = {
-    s3_bucket_uri        = "s3://mymongo"
-    s3_bucket_region     = local.region
-    cron_for_full_backup = "*/2 * * * *"
+    s3_bucket_uri        = "s3://bucket_name"
+    s3_bucket_region     = "bucket_region"
+    cron_for_full_backup = "* * * * *"
   }
   mongodb_restore_enabled = true
   mongodb_restore_config = {
-    s3_bucket_uri              = "s3://mymongo/mongodumpfull_20230424_112501.gz"
-    s3_bucket_region           = "us-east-2"
+    s3_bucket_uri              = "s3://bucket_name/filename"
+    s3_bucket_region           = "bucker_region"
     full_restore_enable        = true
-    file_name_full             = "mongodumpfull_20230424_112501.gz"
+    file_name_full             = "filename"
     incremental_restore_enable = false
     file_name_incremental      = ""
   }
